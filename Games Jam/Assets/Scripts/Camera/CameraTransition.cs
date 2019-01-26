6c6f4a711dabf9d617ecf69da16202ac2501237b
Oliver Chamberlain <ochamberlai2@gmail.com>
2019-01-26 15:16:37 +0000

    Added camera transition

---
+++ Games Jam/Assets/Scripts/Camera/CameraTransition.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTransition : MonoBehaviour
+{
+    [SerializeField]
+    private float transitionSpeed = 2f;
+
+    [SerializeField]
+    private List<Transform> cameraTransforms;
+
+    private int currentTransformIndex = 0;
+    private bool currentlyTransitioning;
+
+    public void TransitionCamera()
+    {
+        int nextTransformIndex = currentTransformIndex + 1;
+
+        if (nextTransformIndex > cameraTransforms.Count)
+        {
+            ResetCamera();
+            return;
+        }
+
+
+        float startTime = Time.time;
+        float journeyLength = Vector3.Distance(cameraTransforms[currentTransformIndex].position, cameraTransforms[nextTransformIndex].position);
+        if(currentlyTransitioning == false)
+        {
+            StartCoroutine(MoveCameraBetweenPoints(cameraTransforms[currentTransformIndex], cameraTransforms[nextTransformIndex], startTime, journeyLength));
+        }
+        currentTransformIndex = nextTransformIndex;
+
+
+    }
+
+    private IEnumerator MoveCameraBetweenPoints(Transform startTransform, Transform destinationTransform, float startTime, float journeyLength)
+    {
+        currentlyTransitioning = true;
+        while(transform.position != destinationTransform.position)
+        {
+            float distanceCovered = (Time.time - startTime) * transitionSpeed;
+            float fractionJourneyCompleted = distanceCovered / journeyLength;
+
+            transform.position = Vector3.Lerp(startTransform.position, destinationTransform.position, fractionJourneyCompleted);
+            yield return null;
+        }
+        currentlyTransitioning = false;
+        yield return null;
+    }
+
+
+    public void ResetCamera()
+    {
+        transform.position = cameraTransforms[0].position;
+        currentTransformIndex = 0;
+    }
+}
