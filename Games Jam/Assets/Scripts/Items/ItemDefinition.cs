d4f6f899f2d679d408e6ab5f6c8afcc51436e85e
Oliver Chamberlain <ochamberlai2@gmail.com>
2019-01-29 20:15:05 +0000

    Added bones of the event system.

--- Games Jam/Assets/Scripts/Items/ItemDefinition.cs
+++ Games Jam/Assets/Scripts/Items/ItemDefinition.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 /*
  * Represents any items that may live in the game world
  */
- [CreateAssetMenu(fileName = "New Item", menuName = "Create New Item")]
+ [CreateAssetMenu(fileName = "New Item", menuName = "Create New Item", order = 3)]
 public class ItemDefinition : ScriptableObject
 {
 
